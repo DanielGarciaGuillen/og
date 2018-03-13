b6494cf828cd28223b76df58fa86131bb0728e59
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2018-03-12 23:17:43 -0400

    tried  react virtualized and it didnt work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
+
+
 import "./App.css";
 
+
 var gifs = [];
+
+var list = [];
+
 
 /* const API = "http://api.giphy.com/v1/gifs/search?q=";
 const DEFAULT_QUERY = "redux";
@@ -15,12 +21,13 @@ class App extends Component {
   }
 
   componentWillMount() {
-    fetch("http://api.giphy.com/v1/gifs/search?q=redux&api_key=dc6zaTOxFJmzC")
+    fetch("http://api.giphy.com/v1/gifs/search?q=design&api_key=dc6zaTOxFJmzC&limit=30")
       .then((response) => response.json())
       .then((data) => {
 
-        gifs = data.data;
-        console.log(gifs)
+        list = data.data;
+        console.log(list);
+        this.setState({ list })
       })
       .catch(error => {
         console.error(error);
@@ -28,35 +35,16 @@ class App extends Component {
   }
 
   render() {
+    const listItems = list.map((gif) => <li className="gif" key={gif.id}>
+      <img className="gifImage" alt={gif.title} src={gif.images.downsized.url} />
+    </li >);
+
     return (
       <div className="main gallery">
-        <div className="gif">
-          <h1>Computer Starter Kit</h1>
-
-          <img
-            src="https://www.w3.org/TR/css-flexbox-1/images/computer.jpg"
-            alt="You get: a white computer with matching peripherals"
-          />
-        </div>
-
-        <div className="gif">
-          <h1>Computer Starter Kit</h1>
-
-          <img
-            src="https://www.w3.org/TR/css-flexbox-1/images/computer.jpg"
-            alt="You get: a white computer with matching peripherals"
-          />
-        </div>
-
-        <div className="gif">
-          <h1>Computer Starter Kit</h1>
-
-          <img
-            src="https://www.w3.org/TR/css-flexbox-1/images/computer.jpg"
-            alt="You get: a white computer with matching peripherals"
-          />
-        </div>
+        {listItems}
       </div>
+
+
     );
   }
 }
