888978daba53a5c015411453e0d2b6953c369ce7
DanielGarciaGuillen <danielgarciaguillen22@gmail.com>
2018-03-14 08:15:15 -0400

    solving problem with a callback on set State not working

--- src/App.js
+++ src/App.js
@@ -10,20 +10,41 @@ const Key = "&api_key=dc6zaTOxFJmzC&limit=30&offset=";
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       list: [],
       offset: 0,
       query: "design"
     };
     this.getGifs = this.getGifs.bind(this);
+    /* this.handleUpdateQuery = this.handleUpdateQuery.bind(this); */
   }
 
+  //API call with Props
+  componentDidMount() {
+    function dataFetech() {
+      fetch(API + `${this.state.query}` + Key + `${this.state.offset}`)
+        .then(response => response.json())
+        .then(data => {
+          list = data.data;
+          this.setState({ list });
+          console.log(this.state);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+
+  //API call to do Infinite Scrolling, firing on  <Waypoint onEnter={this.getGifs}
+  //The funcion also manages the state relaetd to offset, everytime it does
+  //an API call the offset is set to the amount of the calls already done so
+  //the infite scroll doesn't repeat Gifs.
   getGifs() {
     this.setState({ offset: this.state.offset + 30 });
     fetch(API + `${this.state.query}` + Key + `${this.state.offset}`)
       .then(response => response.json())
       .then(data => {
+        //Spread operator to push data into the List array
         list.push(...data.data);
         this.setState({ list });
       })
@@ -32,46 +53,34 @@ class App extends Component {
         console.error(error);
       });
   }
+
+  //Callback from Button Component to update query search
   handleUpdateQuery = query => {
+    list = [];
+    //Set State to prop.query passed by the Button Component, reset arry list and offset.
     this.setState({
       query: query,
-      list: [],
+      list: list,
       offset: 0
     });
-    //Improve design here
+    console.log(this.state);
+    //Call API with new props
     fetch(API + `${this.state.query}` + Key + `${this.state.offset}`)
       .then(response => response.json())
       .then(data => {
         list = data.data;
-        this.setState({ list });
-        console.log(this.state);
+
+        console.log(list);
       })
       .catch(error => {
         console.error(error);
       });
+
+    this.setState({ list: list });
   };
 
-  componentWillMount() {
-    fetch(API + `${this.state.query}` + Key + `${this.state.offset}`)
-      .then(response => response.json())
-      .then(data => {
-        list = data.data;
-        this.setState({ list });
-        console.log(this.state);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
-  /* componentWillReceiveProps(nextProps) {
-    if ((this.props.query) !== (nextProps.query)) {
-      console.log("new props!")
-      this.getGifs();
-    }
-  }
- */
   render() {
+    //Mapping through the list array.
     const listItems = list.map(gif => (
       <li className="gif" key={gif.id}>
         <a className="linkGif" href={gif.url} target="_blank">
@@ -85,7 +94,9 @@ class App extends Component {
     ));
 
     return (
+      //GRID
       <div className="container">
+        {/* //Header */}
         <div className="header" id="animationBackground" />
 
         <ChangeTheme onClick={this.handleUpdateQuery} />
--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+var query = "";
 
 export class ChangeTheme extends React.Component {
   constructor(props) {
@@ -7,56 +8,72 @@ export class ChangeTheme extends React.Component {
     this.state = {
       text: ""
     };
-    this.handleClick = this.handleClick.bind(this);
+    /*  this.handleClick = this.handleClick.bind(this); */
+    this.changeQuery = this.changeQuery.bind(this);
+    this.validateTitle = this.validateTitle.bind(this);
   }
 
-  handleClick({ currentTarget }) {
+  changeQuery({ currentTarget }) {
+    query = currentTarget.value;
+    this.setState({ text: query }, function() {
+      this.validateTitle();
+    });
+  }
+  validateTitle() {
+    const { onClick } = this.props;
+    onClick(query);
+    console.log(this.props);
+    console.log(this.state);
+  }
+
+  /*  handleClick({ currentTarget }) {
     const query = currentTarget.value;
     this.setState({ text: query });
-    const { onClick } = this.props;
 
     console.log(this.props);
     console.log(this.state);
     //On Submit is coming from the app component binded to handleUpdateLocation
+    const { onClick } = this.props;
     onClick(query);
-  }
+  } */
 
   render() {
+    const { query } = this.state;
     return (
       <div className="buttons">
-        <button className="button" value="design" onClick={this.handleClick}>
+        <button className="button" value="design" onClick={this.changeQuery}>
           Design
         </button>
-        <button className="button" value="motion" onClick={this.handleClick}>
+        <button className="button" value="motion" onClick={this.changeQuery}>
           Motion
         </button>
         <button
           className="button"
           value="architecture"
-          onClick={this.handleClick}
+          onClick={this.changeQuery}
         >
           Architecture
         </button>
-        <button className="button" value="3d" onClick={this.handleClick}>
+        <button className="button" value="3d" onClick={this.changeQuery}>
           3d
         </button>
         <button
           className="button"
           value="digital%20art"
-          onClick={this.handleClick}
+          onClick={this.changeQuery}
         >
           Digital Art
         </button>
-        <button className="button" value="pixelart" onClick={this.handleClick}>
+        <button className="button" value="pixelart" onClick={this.changeQuery}>
           Pixel Art
         </button>
-        <button className="button" value="animation" onClick={this.handleClick}>
+        <button className="button" value="animation" onClick={this.changeQuery}>
           Animation
         </button>
-        <button className="button" value="render" onClick={this.handleClick}>
+        <button className="button" value="render" onClick={this.changeQuery}>
           Render
         </button>
-        <button className="button" value="art" onClick={this.handleClick}>
+        <button className="button" value="art" onClick={this.changeQuery}>
           Art
         </button>
       </div>
